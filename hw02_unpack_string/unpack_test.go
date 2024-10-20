27e891fc0258a0aa2b7929204e6672e8edcabd2a
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2024-10-20 03:17:06 +0300

    test(HW2):  upg functional tests, introduce tests for sub-func.

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -1,4 +1,4 @@
-package main
+package hw02unpackstring
 
 import (
 	"errors"
@@ -21,6 +21,7 @@ func TestUnpack(t *testing.T) {
 		{input: `qwe\45`, expected: `qwe44444`},
 		{input: `qwe\\5`, expected: `qwe\\\\\`},
 		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `ёghhб2`, expected: `ёghhбб`},
 	}
 
 	for _, tc := range tests {
@@ -33,11 +34,40 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", `d\nabc`, `qw\ne\`, `ghh\б2`}
 	for _, tc := range invalidStrings {
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
 		})
 	}
+}
+
+func TestIsDigit(t *testing.T) {
+	for r := '1'; r <= '9'; r++ {
+		require.True(t, isDigit(r))
+	}
+	for r := 'a'; r <= 'z'; r++ {
+		require.False(t, isDigit(r))
+	}
+	for r := 'A'; r <= 'Z'; r++ {
+		require.False(t, isDigit(r))
+	}
+}
+
+func TestIsForbidden(t *testing.T) {
+	require.True(t, isForbidden([]rune{'\\', 'a'}, 0))
+	require.False(t, isForbidden([]rune{'\\', '1'}, 0))
+	require.False(t, isForbidden([]rune{'\\', '\\'}, 0))
+}
+
+func TestIsMultiplyDigit(t *testing.T) {
+	require.True(t, isMultiplyDigit([]rune{'\\', '4', '5'}, 0, 3))
+	require.False(t, isMultiplyDigit([]rune{'\\', '4', 'a'}, 0, 3))
+	require.False(t, isMultiplyDigit([]rune{'\\', '4', '\\'}, 0, 3))
+	require.False(t, isMultiplyDigit([]rune{'\\', '4'}, 0, 2))
+}
+
+func TestIsSingleDigit(t *testing.T) {
+	require.True(t, isSingleDigit([]rune{'\\', '4', 'a'}, 0))
 }
