46fcdaf164df1a6bb483231caee55394a719a518
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2024-10-29 02:31:34 +0300

    feat(HW3): solved simple var

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,75 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"strings"
+
+	"sort"
+)
+
+func Top10(s string) []string {
 	// Place your code here.
-	return nil
+	slices := sliceStr(s)
+	counter := countWords(slices)
+	top10 := takeTop(counter, 10)
+	return top10
+}
+
+func sliceStr(s string) []string {
+	return strings.Fields(s) // strings.Split(s, " ") //
+}
+
+func countWords(slices []string) map[string]int {
+	m := make(map[string]int)
+	for _, r := range slices {
+		m[r]++
+	}
+	return m
+}
+
+func takeTop(counter map[string]int, n int) []string {
+	var ordered = sortKeys(counter)
+	return trim(ordered, n)
+}
+
+func sortKeys(counter map[string]int) []string {
+	keys := make([]string, 0, len(counter))
+	for key := range counter {
+		keys = append(keys, key)
+	}
+
+	sortLex(keys)
+	sortVal(keys, counter)
+	return keys
+}
+
+// value sorting
+func sortVal(keys []string, counter map[string]int) {
+	sort.Slice(keys, func(i, j int) bool {
+		if counter[keys[i]] == counter[keys[j]] {
+			return keys[i] < keys[j]
+		}
+		return counter[keys[i]] > counter[keys[j]]
+	})
+}
+
+//Expected :[]string{"он", "а", "и", "ты", "что", "-", "Кристофер", "если", "не", "то"}
+//Actual   :[]string{"он", "а", "и", "ты", "что", "то", "Кристофер", "не", "если", "-"}
+
+// lexicographic sorting
+func sortLex(keys []string) {
+	sort.Strings(keys)
+}
+
+func trim(ordered []string, n int) []string {
+	var res []string
+	var count int
+	for _, s := range ordered {
+		if count >= n {
+			break
+		}
+		res = append(res, s)
+		count++
+
+	}
+	return res
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -1,6 +1,7 @@
 package hw03frequencyanalysis
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -78,5 +79,103 @@ func TestTop10(t *testing.T) {
 			}
 			require.Equal(t, expected, Top10(text))
 		}
+	})
+}
+
+func TestSplit(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected []string
+	}{
+		{input: "some one", expected: []string{"some", "one"}},
+		{input: "some,one", expected: []string{"some,one"}},
+		{input: "some one ", expected: []string{"some", "one", ""}},
+		{input: ",some one", expected: []string{",some", "one"}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input, func(t *testing.T) {
+			result := sliceStr(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestCountWords(t *testing.T) {
+	tests := []struct {
+		input    []string
+		expected map[string]int
+	}{
+		{input: []string{"some", "one"}, expected: map[string]int{"some": 1, "one": 1}},
+		{input: []string{"some", "some", "one"}, expected: map[string]int{"some": 2, "one": 1}},
+		{input: []string{"some", "one", "a", "", ""}, expected: map[string]int{"some": 1, "one": 1, "a": 1, "": 2}},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.input[0], func(t *testing.T) {
+			result := countWords(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestSortKeys(t *testing.T) {
+	tests := []struct {
+		input    map[string]int
+		expected []string
+	}{
+		{input: map[string]int{"some": 1, "one": 2}, expected: []string{"one", "some"}},
+		{input: map[string]int{"a": 2, "b": 3, "c": 1}, expected: []string{"b", "a", "c"}},
+		{input: map[string]int{"a": 2, "b": 2, "c": 1}, expected: []string{"a", "b", "c"}},
+		{input: map[string]int{"b": 2, "a": 2, "c": 1}, expected: []string{"a", "b", "c"}},
+	}
+
+	for _, tc := range tests {
+		name := func(m map[string]int) string {
+			var s strings.Builder
+			for key := range m {
+				s.WriteString(key)
+			}
+			return s.String()
+		}(tc.input)
+		t.Run(name, func(t *testing.T) {
+			result := sortKeys(tc.input)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestTrim(t *testing.T) {
+	var data []string
+	for r := 'a'; r <= 'z'; r++ {
+		data = append(data, string(r))
+	}
+	expected := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+	result := trim(data, 10)
+	require.Equal(t, expected, result)
+}
+
+//func TestTakeTop(t *tesing.T) {
+//	tests := []struct {
+//		input 	 map[string]int
+//		expected []string
+//	}{
+//		{input: map[string]int{"some": 1, "one": 1}, expected: []string{}},
+//	}
+//}
+
+func TestLexSort(t *testing.T) {
+	t.Run("positive test", func(t *testing.T) {
+		expected := []string{
+			"-",         // 4
+			"Кристофер", // 4
+			"если",      // 4
+			"не",        // 4
+			"то",        // 4
+		}
+		result := make([]string, len(expected))
+		copy(result, expected)
+		sortLex(result)
+		require.Equal(t, expected, result)
 	})
 }
