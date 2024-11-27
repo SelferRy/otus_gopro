7ef631c6833aebffa8c64822ebf592ce80bd1f99
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2024-11-28 01:14:58 +0300

    feat(HW6): solved basic ver

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,34 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return in
+	}
+	for _, stage := range stages {
+		if stage != nil {
+			in = stage(doneStage(in, done))
+		}
+	}
+	return in
+}
+
+func doneStage(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- val
+			}
+		}
+	}()
+
+	return out
 }
