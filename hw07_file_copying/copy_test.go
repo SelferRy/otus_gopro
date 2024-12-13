62977edcb6d9aac9a7da26d73a1e4df892f31981
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2024-12-14 01:06:25 +0300

    test(HW7): add tests

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,67 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("copy /dev/urandom", func(t *testing.T) {
+		err := Copy("/dev/urandom", "/tmp", 0, 0)
+		require.Equal(t, ErrUnsupportedFile, errors.Unwrap(err))
+	})
+	t.Run("file doesn't exist", func(t *testing.T) {
+		err := Copy("test.txt", "/tmp", 0, 0)
+		require.NotNil(t, err)
+	})
+	t.Run("copy directory", func(t *testing.T) {
+		err := Copy("/tmp", "test.txt", 0, 0)
+		require.Equal(t, ErrUnsupportedFile, errors.Unwrap(err))
+	})
+	t.Run("copy to directory", func(t *testing.T) {
+		_, _ = os.Create("test.txt")
+		defer func() {
+			if err := os.Remove("test.txt"); err != nil {
+				slog.Any("error", err)
+			}
+		}()
+		err := Copy("test.txt", "/tmp", 0, 0)
+		require.Equal(t, ErrEmptyFile, errors.Unwrap(errors.Unwrap(err)))
+	})
+	t.Run("permissions", func(t *testing.T) {
+		_, _ = os.Create("test1.txt")
+		defer func() {
+			if err := os.Remove("test1.txt"); err != nil {
+				slog.Any("error", err)
+			}
+		}()
+
+		_, _ = os.Create("test2.txt")
+		defer func() {
+			if err := os.Remove("test2.txt"); err != nil {
+				slog.Any("error", err)
+			}
+		}()
+
+		_ = os.Chmod("test2.txt", 0444)
+
+		err := Copy("test1.txt", "test2.txt", 0, 0)
+		require.NotNil(t, err)
+	})
+	t.Run("copy file to itself", func(t *testing.T) {
+		f, _ := os.Create("test.txt")
+		defer func() {
+			if err := os.Remove("test.txt"); err != nil {
+				slog.Any("error", err)
+			}
+		}()
+		_, _ = f.WriteString("test")
+
+		err := Copy("test.txt", "test.txt", 0, 0)
+		require.NotNil(t, err)
+	})
 }
