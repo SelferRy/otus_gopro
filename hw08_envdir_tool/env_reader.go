452b0fb65eb097fb1fb95fc2515d6c9b894da1de
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2024-12-20 01:45:16 +0300

    feat(HW8): init HW

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +19,37 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	env := make(Environment, len(files))
+	for _, file := range files {
+		fmt.Println(file.Name())
+		env[file.Name()] = defineEnvVal(file.Name())
+
+	}
 	return nil, nil
+}
+
+func defineEnvVal(fileName string) EnvValue {
+	file, err := os.Open(fileName)
+	if err != nil {
+		slog.Error("error with os.Open", err)
+	}
+	defer func() {
+		err := file.Close()
+		slog.Error("error with os.File().Close", err)
+	}()
+	val := func() string {
+		reader := bufio.NewReader(file)
+		str, _, err := reader.ReadLine()
+		if err != nil {
+			slog.Error("error with bufio.NewReader(file).ReadLine%w", err)
+		}
+		return string(str)
+	}()
+
+	return EnvValue{val, false}
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
 	// Place your code here
+	t.Run("read directory", func(t *testing.T) {
+		target := Environment{
+			"BAR":   EnvValue{"bar", false},
+			"EMPTY": EnvValue{"", false},
+			"FOO":   EnvValue{"   foo", false},
+			"HELLO": EnvValue{"foo", false},
+			"UNSET": EnvValue{"", true},
+		}
+		result, err := ReadDir("./testdata/env/")
+		require.NoError(t, err)
+		require.Equal(t, target, result)
+	})
+}
+
+func TestDefineEnvVal(t *testing.T) {
+	// Place your code here
+	t.Run("read directory", func(t *testing.T) {
+		target := EnvValue{"bar", false}
+		result := defineEnvVal("./testdata/env/BAR")
+		require.Equal(t, target, result)
+	})
 }
