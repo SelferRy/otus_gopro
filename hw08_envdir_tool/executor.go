85bf3d744bcd988fa1fed99a8487e75893839c08
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2024-12-27 01:27:20 +0300

    fix(HW8): read env vars

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"log/slog"
 	"os"
 	"os/exec"
 )
@@ -19,13 +21,31 @@ func RunCmd(cmd []string, env Environment) (returnCode int) {
 	command, args := cmd[0], cmd[1:]
 	execCmd := exec.Command(command, args...)
 	execCmd.Stdin, execCmd.Stdout, execCmd.Stderr = os.Stdin, os.Stdout, os.Stderr
-	execCmd.Env = env.StringSlice()
-	if err := execCmd.Run(); err != nil {
+	err := setupEnv(env)
+	if err = execCmd.Run(); err != nil {
+		slog.Error("execution error", slog.Any("error:", err))
 		var exitErr *exec.ExitError
 		if errors.As(err, &exitErr) {
 			return exitErr.ExitCode()
 		}
 		return ExitCodeCommandInvokedCannotExecute
 	}
-	return 0
+	return execCmd.ProcessState.ExitCode()
+}
+
+// change environment variable
+func setupEnv(env Environment) error {
+	for key, val := range env {
+		if val.NeedRemove {
+			err := os.Unsetenv(key)
+			if err != nil {
+				return fmt.Errorf("os.Unsetenv problem.\n%w", err)
+			}
+		}
+		err := os.Setenv(key, val.Value)
+		if err != nil {
+			return fmt.Errorf("os.Setenv problem.\n%w", err)
+		}
+	}
+	return nil
 }
