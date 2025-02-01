a407e44739a66497020a4f49ee02ae38fbe8f18e
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2025-02-01 19:34:07 +0300

    feat(HW11): imp telnet client

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log/slog"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,48 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+type client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *client) Connect() (err error) {
+	if c.conn != nil {
+		slog.Error("already connected")
+		return nil
+	}
+	c.conn, err = net.DialTimeout("tcp", c.address, c.timeout)
+	slog.Info(fmt.Sprintf("...connected to %s\n", c.address))
+	return err
+}
+
+func (c *client) Close() error {
+	if c.conn == nil {
+		slog.Error("not connected")
+		return nil
+	}
+	return c.conn.Close()
+}
+
+func (c *client) Send() error {
+	_, err := io.Copy(c.conn, c.in)
+	if err != nil {
+		slog.Error(fmt.Sprintf("error during sending: %s", err.Error()))
+	}
+	return err
+}
+
+func (c *client) Receive() error {
+	_, err := io.Copy(c.out, c.conn)
+	if err != nil {
+		slog.Error(fmt.Sprintf("error during receving: %s", err.Error()))
+	}
+	return err
+}
+
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &client{address: address, timeout: timeout, in: in, out: out}
+}
